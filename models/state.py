b97b294ec583983e4f2aa17a6e0d3a43c7e4ef00
Erickkaranja <karanja99erick@gmail.com>
2023-04-19 23:02:15 +0300

    adding task 6 & 7

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from os import getenv
+from models.base_model import Base
+from models.city import City
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, base):
+    """ Represents a state for a mysql database
+
+    inherits from SQLalchemy Base and links to mysql table states.
+    Attributes:
+        __tablename__ (str) : The name of the mysql table to store states.
+        name (sqlalchemy String) :The name of the state.
+        city(sqlalchemy relationship): the state city relationship.
+    """
+    __tablename__ = "states"
+    name = column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related cities."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id = self.id:
+                    city_list.append(city)
+        return city_list
--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
+    inherits from mysqlbase and links to mysql table users.
+
+    Attributes:
+        __tablename__ (str): name of mysql table to store users.
+        email (sqlalchemy String) : Email of the user.
+        password (sqlalchemy String) : passwd of the user.
+        first_name (sqlalchemy String) : firstname of the user.
+        last_name (sqlalchemy String) : lastname of the user.
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
