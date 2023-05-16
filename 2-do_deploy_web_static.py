662fb0912343e16e1b5ea4cfaa1e40fd25472985
sezfabian <cheruiyotfabian@gmail.com>
2023-05-16 19:24:41 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -34,14 +34,15 @@ def do_deploy(archive_path):
 
         # Delete the archive from the web server
         run('rm /tmp/{}'.format(filename))
-        run('mv -f /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(folder_extract, folder_extract))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_extract))
+        run('mv -f /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(filename_no_ext, filename_no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(filename_no_ext))
         
         # Delete symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
 
         # Create a new the symbolic link /data/web_static/current
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename_no_ext))
+        run('sudo systemctl restart nginx')
         
         print('New version deployed!')
         return True
