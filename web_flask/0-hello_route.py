8678e8fd79ca6aa9fec153f02a6e83d7f9d4c262
sezfabian <cheruiyotfabian@gmail.com>
2023-05-21 08:12:18 +0300

    Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 script that starts a Flask web application:
+    application must be listening on 0.0.0.0, port 5000
+    
 """
 from flask import Flask
 app = Flask(__name__)
