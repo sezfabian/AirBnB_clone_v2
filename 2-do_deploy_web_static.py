eb83169f76496a0a56a26d400fa4089fa3fbde7f
sezfabian <cheruiyotfabian@gmail.com>
2023-05-22 02:59:43 +0300

    states

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from os.path import exists
 import os
 
 # Remote servers
-env.hosts = ['52.86.36.245', '100.25.45.160']
+env.hosts = ['100.26.223.28']
 
 
 def do_deploy(archive_path):
@@ -26,22 +26,22 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         # Create the folder /data/web_static/releases/<filename_no_ext>
-        run('mkdir -p /data/web_static/releases/{}/'.format(filename_no_ext))
+        run('sudo mkdir -p /data/web_static/releases/{}/'.format(filename_no_ext))
 
         # Uncompress the archive to the folder
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
             .format(filename, filename_no_ext))
 
         # Delete the archive from the web server
-        run('rm /tmp/{}'.format(filename))
-        run('mv -f /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(filename_no_ext, filename_no_ext))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(filename_no_ext))
+        run('sudo rm /tmp/{}'.format(filename))
+        run('sudo mv -f /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(filename_no_ext, filename_no_ext))
+        run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(filename_no_ext))
         
         # Delete symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
 
         # Create a new the symbolic link /data/web_static/current
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename_no_ext))
+        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename_no_ext))
         run('sudo systemctl restart nginx')
         
         print('New version deployed!')
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,9 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """
+        reload for decerializing JSON file to objects
+        """
+        self.reload()
